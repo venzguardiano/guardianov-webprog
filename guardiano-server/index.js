d79f8288fa629f5fdafcb524f4bcaca7fbe18cd1
venzguardiano <09163757471a@gmail.com>
2026-05-29 21:41:22 +0800

    setup for vercel deployment

--- guardiano-server/index.js
+++ guardiano-server/index.js
@@ -56,5 +56,4 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: "Server Error" });
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
